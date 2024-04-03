c198b3a7304324744dafa5fd0f38cdecdc8145bf
officialkimuel <84969307+officialkimuel@users.noreply.github.com>
2024-04-03 21:49:20 +0800

    6
    6th modified

--- react-portfolio-main/src/components/Contact/Contact.jsx
+++ react-portfolio-main/src/components/Contact/Contact.jsx
@@ -13,7 +13,7 @@ export const Contact = () => {
       <ul className={styles.links}>
         <li className={styles.link}>
           <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
-          <a href="mailto:officialkimuel9@email.com">officialkimuel9@email.com</a>
+          <a href="mailto:officialkimuel9@email.com">officialkimuel9@gmail.com</a>
         </li>
         <li className={styles.link}>
           <img
