9c0e6ee895d0442f68f939b6e7c3a1575be2f982
officialkimuel <84969307+officialkimuel@users.noreply.github.com>
2024-04-04 18:47:00 +0800

    9
    9th modified

--- react-portfolio-main/src/components/Hero/Hero.jsx
+++ react-portfolio-main/src/components/Hero/Hero.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 import styles from "./Hero.module.css";
 import { getImageUrl } from "../../utils";
 
@@ -9,7 +8,7 @@ export const Hero = () => {
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Kimuel Lagnason</h1>
         <p className={styles.description}>
-        As a 22-year-old Information Technology student at Saint Columban College in Pagadian City, I'm currently enhancing my skills as a Front-End Developer intern at Studio Pinya. I'm focused on contributing innovative design solutions and employing the latest web technologies for our team's success.
+          As a 22-year-old Information Technology student at <span className={styles.glowCollege}>Saint Columban College</span> in Pagadian City, I'm currently enhancing my skills as a <span className={styles.glowDeveloper}>Front-End Developer</span> intern at <span className={styles.glowStudio}>Studio Pinya</span>. I'm focused on contributing innovative design solutions and employing the latest web technologies for our team's success.
         </p>
         <a href="mailto:officialkimuel9@email.com" className={styles.contactBtn}>
           Contact Me
@@ -23,6 +22,5 @@ export const Hero = () => {
       <div className={styles.topBlur} />
       <div className={styles.bottomBlur} />
     </section>
-    
   );
 };
--- react-portfolio-main/src/components/School/School.jsx
+++ react-portfolio-main/src/components/School/School.jsx
@@ -5,26 +5,78 @@ import { getImageUrl } from "../../utils";
 export const School = () => {
   return (
     <section className={styles.container} id="school">
-      <h2 className={styles.title} style={{ marginBottom: '20px' }}>School & Department</h2>
-      <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around', gap: '20px' }}>
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', maxWidth: '45%' }}>
+      <h2 className={styles.title} style={{ textAlign: "center" }}>
+        School & Department
+      </h2>
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "space-around",
+          gap: "20px",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            maxWidth: "45%",
+          }}
+        >
           <img
             src={getImageUrl("school/scc.png")}
             alt="Saint Columban College"
             className={styles.image}
+            style={{ marginTop: "-80px", width: "auto", height: "auto" }}
           />
-          <p className={styles.imageDescription} style={{ marginTop: '10px', marginBottom: '10px' }}>Saint Columban College</p>
-          <p style={{ textAlign: 'center' ,fontSize:'20px'}}>Known to be one of the most leading institutions in Pagadian City, Saint Columban College, in its pursuit of academic excellence and character formation, has taken on the spotlight of producing globally-competitive graduates since its founding. As an institution since 1957, SCC offers the complete educational package from kindergarten to graduate school. Withstanding every challenge and adversities for 64 years and counting; SCC became not just an institution but a home to many SCCians.</p>
+          <p
+            className={styles.imageDescription}
+            style={{ marginTop: "-90px", marginBottom: "10px" }}
+          >
+            Saint Columban College
+          </p>
+          <p style={{ textAlign: "center", fontSize: "20px" }}>
+            Known to be one of the most leading institutions in Pagadian City,
+            Saint Columban College, in its pursuit of academic excellence and
+            character formation, has taken on the spotlight of producing
+            globally-competitive graduates since its founding. As an institution
+            since 1957, SCC offers the complete educational package from
+            kindergarten to graduate school. Withstanding every challenge and
+            adversities for 64 years and counting; SCC became not just an
+            institution but a home to many SCCians.
+          </p>
         </div>
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', maxWidth: '45%', marginTop: '-20px' }}>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            maxWidth: "45%",
+          }}
+        >
           <img
             src={getImageUrl("school/ccs.png")}
             alt="College of Computer Studies"
             className={styles.image}
-            style={{ width: '312px', height: 'auto' }}
+            style={{ marginTop: "-80px", width: "auto", height: "auto" }}
           />
-          <p className={styles.imageDescription} style={{ marginTop: '10px', marginBottom: '10px' }}>College of Computing Studies</p>
-          <p style={{ textAlign: 'center',fontSize:'20px' }}>Established in 1992, the College of Computing Studies Department at Saint Columban College became a standout department in the 1990s. Offering degrees such as BSIT, BSCS, BLIS, and BSIS, it catered to the burgeoning need for tech professionals. Known for blending theory with practical skills, the department prepared students for the fast-evolving tech industry. Its reputation for excellence in computing and information technology education remains a cornerstone of Saint Columban College.</p>
+          <p
+            className={styles.imageDescription}
+            style={{ marginTop: "-90px", marginBottom: "10px" }}
+          >
+            College of Computing Studies
+          </p>
+          <p style={{ textAlign: "center", fontSize: "20px" }}>
+            Established in 1992, the College of Computing Studies Department at
+            Saint Columban College became a standout department in the 1990s.
+            Offering degrees such as BSIT, BSCS, BLIS, and BSIS, it catered to
+            the burgeoning need for tech professionals. Known for blending
+            theory with practical skills, the department prepared students for
+            the fast-evolving tech industry. Its reputation for excellence in
+            computing and information technology education remains a cornerstone
+            of Saint Columban College.
+          </p>
         </div>
       </div>
     </section>
